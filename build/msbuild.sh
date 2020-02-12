e46450507fff421041526e4f8f41657a87401d1b
MarkS <marksvc@gmail.com>
2020-02-12 14:44:47 -0600

    Change packaging to use mono 5
    * Change depends and build-depends to mono5-sil and libgdiplus5-sil to
    more easily run on Ubuntu 20.04.
    * Add msbuild dependency.
    * Put $@ first in debian/rules dh command to be ready for future
    compatibility levels of dh.
    * In debian/rules, use environ to set the environment over all the
    packaging steps.
    - Removing FIELDWORKS=1 since is in environ.
    * Give msbuild.sh output some context.
    * Newer nuget seems to need more help than just XDG_CONFIG_HOME or
    XDG_DATA_HOME. Setting HOME instead since otherwise HOME is
    /nonexistent, causing a problem for nuget downloading.
    * Reorder the Release section of a couple csproj files so `make
    release` works.
    * Broaded dependency to python2-dev, still in Ubuntu 20.04 universe.
    - Set PYTHON_VERSION="2.7" in `environ` so configure finds the
    `python2.7` executable instead of looking for `python` and not finding
    it.
    * Make downloading with wget and nuget be retried a few times, so that
    network problems on the build server are less likely to fail a job.

--- build/msbuild.sh
+++ build/msbuild.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+# Run msbuild
+#
 # $1 is the value of the MONO variable set by configure
 # $2 is the value of the MONO_LDFLAGS variable set by configure
-# any following arguments are passed on to xbuild proper
-# build with /usr/local/bin/mono on developer machines if it's the default
+# Any following arguments are passed on.
+
+log() {
+  echo msbuild.sh[$$]: "$@"
+}
+
+# Build with /usr/local/bin/mono on developer machines if it's the default
 if [ "$(which mono)" = "/usr/local/bin/mono" ]; then
-	echo adjusting PATH and LD_LIBRARY_PATH
+	log Adjusting PATH and LD_LIBRARY_PATH
 	export PATH=/usr/local/bin:$PATH
 	export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 elif [ "$1" != "$(which mono)" ]; then
-	echo adjusting PATH and LD_LIBRARY_PATH
+	log Adjusting PATH and LD_LIBRARY_PATH
 	export PATH=$(dirname $1):$PATH
 	# note that MONO_LDFLAGS starts with -L, which we don't want
 	export LD_LIBRARY_PATH=$(echo $2|cut -c3-):$LD_LIBRARY_PATH
 fi
-echo PATH=$PATH
-echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+log PATH=$PATH
+log LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 shift 2
 msbuild "$@"
