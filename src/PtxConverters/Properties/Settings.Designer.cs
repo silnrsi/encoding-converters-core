9e3c25e62075604bf1ac24a75ffbe4cb09986e8c
bobeaton <beaton@bootbarn.com>
2025-03-19 17:14:06 -0400

    added \fr (footnote ref), and cross reference markers to the PtxProjectConverter

--- src/PtxConverters/Properties/Settings.Designer.cs
+++ src/PtxConverters/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SilEncConverters40.PtxConverters.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -27,9 +27,13 @@ namespace SilEncConverters40.PtxConverters.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
 <ArrayOfString xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">
+  <string>fr</string>
   <string>fq</string>
   <string>ft</string>
   <string>fp</string>
+  <string>xo</string>
+  <string>xq</string>
+  <string>xt</string>
   <string>rq</string>
   <string>s</string>
   <string>ms1</string>
